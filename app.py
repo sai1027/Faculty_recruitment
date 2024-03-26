df56285acaaaf4629cc262d9ff1b42c9ce8dcf89
sai teja <venkatsaiteja1027@gmail.com>
2024-03-27 04:45:34 +0530

    optimized

--- app.py
+++ app.py
@@ -5,17 +5,11 @@ import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from src.pipeline.predict_pipeline import CustomData,PredictPipeline
 
-application=Flask(__name__)
 
-app=application
+app = Flask(__name__)
 
 ## Route for a home page
-
-@app.route('/')
-def index():
-    return render_template('index.html') 
-
-@app.route('/predictdata',methods=['GET','POST'])
+@app.route('/',methods=['GET','POST'])
 def predict_datapoint():
     if request.method=='GET':
         return render_template('home.html')
@@ -53,5 +47,5 @@ def download_excel():
 
 if __name__=="__main__":
     app.run(host="0.0.0.0")        
-
+    
 
